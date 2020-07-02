3908b1dda8c3aa509fef86e9b0ed8006c2932ecf
shirtiny <1849468240@qq.com>
2020-07-02 20:50:15 +0800

    列表

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,38 +1,75 @@
 <template>
-  <div class="container">
+  <div class="containerBox">
     <div class="innerBox">
-      <div class="boxTitle">
-        mpvue小程序
-      </div>
-      <div class="boxContent">
-        内容
+      <div class="titleBox">subEditor</div>
+      <div class="contentBox">
+        <div class="row" v-for="(subFile,i) in subFiles" :key="i">
+          <label class="left">
+            <img class="img" src="../../../static/tabs/file.png" alt="文件" />
+            <span class="fileName">{{subFile.fileName}}</span>
+          </label>
+          <label class="rightText">
+            <span>></span>
+          </label>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      subFiles: [{ fileName: "name.vtt" }, { fileName: "qy.vtt" }]
+    };
+  }
+};
+</script>
 
 <style lang="scss" scoped>
-.container {
+.containerBox {
   display: flex;
   height: 100vh;
   width: 100vw;
   justify-content: center;
   align-items: center;
+  flex-flow: column nowrap;
   .innerBox {
+    flex:1;
+    width: 100%;
     display: flex;
     flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    .boxTitle {
-      font-weight: bold;
-      font-size: 24px;
-      margin-bottom: 30vh;
+    .titleBox {
+      text-align: center;
+      margin: 30px;
+      font-size: 30px;
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+      font-style: italic;
+      color: #66cccc;
     }
-    .boxContent {
-      justify-content: center;
-      align-items: center;
+    .contentBox {
+      .rightText {
+        clear: right;
+        float: right;
+        height: 55px;
+        color: #E8E8E8;
+      }
+      .row {
+        height: 55px;
+        border-bottom: 1px #E8E8E8 solid;
+        line-height: 70px;
+        padding: 0 20px;
+        .img {
+          width: 30px;
+          height: 30px;
+          float: left;
+          padding-top: 20px;
+        }
+        .fileName {
+          margin-left: 10px;
+        }
+      }
     }
   }
 }
